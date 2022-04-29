f568763c688009575a647594471af9096da00ecb
modelearth <model@dreamstudio.com>
2022-04-29 11:44:18 -0400

    scroll to impact IO

--- js/apps-menus.js
+++ js/apps-menus.js
@@ -1,3 +1,9 @@
+function scrollToImpacts() {
+    $('html,body').animate({
+        scrollTop: $("#impactTopHolder").offset().top - 54
+    });
+}
+
 function showImpactWidget(menuType, heroMenu, site) {
     var folder = '';
 
@@ -28,12 +34,12 @@ function showImpactWidget(menuType, heroMenu, site) {
 
     html[i++] = "<style>.impactIcons span{float:left;font-size:16px;padding-right:8px}</style>";
     html[i++] = "<div class='impactIcons'>";
-    html[i++] = "<div class='impactIcon_Air' onclick=\"goHash({'set':'air','indicators':'GHG,GCC,MGHG,OGHG,HRSP,OZON,SMOG,HAPS'});\"><span class='material-icons'>air</span> <span>Air</span></div>";
-    html[i++] = "<div class='impactIcon_Water' onclick=\"goHash({'set':'water','indicators':'WATR,ACID,EUTR,ETOX'});\"><span class='material-icons'>water_drop</span> <span>Water</span></div>";
-    html[i++] = "<div class='impactIcon_Land' onclick=\"goHash({'set':'land','indicators':'LAND,MNRL,PEST,METL,CRHW,CMSW,FMSW,CCDD'});\"><span class='material-icons'>forest</span> <span>Land</span></div>";
-    html[i++] = "<div class='impactIcon_Energy' onclick=\"goHash({'set':'energy','indicators':'ENRG,NNRG,RNRG'});\"><span class='material-icons'>lightbulb</span> <span>Energy</span></div>";
-    html[i++] = "<div class='impactIcon_Health' onclick=\"goHash({'set':'health','indicators':'HTOX,HCAN,HNCN,HTOX,HRSP'});\"><span class='material-icons'>local_pharmacy</span> <span>Health</span></div>";
-    html[i++] = "<div class='impactIcon_Prosperity' onclick=\"goHash({'set':'prosperity','indicators':'VADD,JOBS'});\"><span class='material-icons'>sentiment_satisfied_alt</span> <span>Prosperity</span></div>";
+    html[i++] = "<div class='impactIcon_Air' onclick=\"goHash({'set':'air','indicators':'GHG,GCC,MGHG,OGHG,HRSP,OZON,SMOG,HAPS'});scrollToImpacts();\"><span class='material-icons'>air</span> <span>Air</span></div>";
+    html[i++] = "<div class='impactIcon_Water' onclick=\"goHash({'set':'water','indicators':'WATR,ACID,EUTR,ETOX'});scrollToImpacts();\"><span class='material-icons'>water_drop</span> <span>Water</span></div>";
+    html[i++] = "<div class='impactIcon_Land' onclick=\"goHash({'set':'land','indicators':'LAND,MNRL,PEST,METL,CRHW,CMSW,FMSW,CCDD'});scrollToImpacts();\"><span class='material-icons'>forest</span> <span>Land</span></div>";
+    html[i++] = "<div class='impactIcon_Energy' onclick=\"goHash({'set':'energy','indicators':'ENRG,NNRG,RNRG'});scrollToImpacts();\"><span class='material-icons'>lightbulb</span> <span>Energy</span></div>";
+    html[i++] = "<div class='impactIcon_Health' onclick=\"goHash({'set':'health','indicators':'HTOX,HCAN,HNCN,HTOX,HRSP'});scrollToImpacts();\"><span class='material-icons'>local_pharmacy</span> <span>Health</span></div>";
+    html[i++] = "<div class='impactIcon_Prosperity' onclick=\"goHash({'set':'prosperity','indicators':'VADD,JOBS'});scrollToImpacts();\"><span class='material-icons'>sentiment_satisfied_alt</span> <span>Prosperity</span></div>";
     html[i++] = "</div>";
 
     $(document).ready(function() {
